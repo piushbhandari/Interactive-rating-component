40530c91844a66d80c08f2591b4baa1a730b0a0a
piush bhandari <piushthegreat@piushs-MacBook-Pro.local>
2022-08-01 13:59:10 -0500

    added js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 const ratingBtns = [...document.querySelectorAll(".card__rating-button")];
+const submitBtn = document.querySelector(".card__submit-btn");
+
+let rating = 1;
+
+ratingBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const target = e.target;
+    const value = target.value;
+
+    // remove style for the selected button on all buttons
+    clearStyles();
+
+    target.classList.add("chosen-btn");
+    target.disabled = true;
+    rating = value;
+  });
+});
+
+submitBtn.addEventListener("click", (e) => {
+  console.log("hi");
+});
+function clearStyles() {
+  ratingBtns.forEach((btn) => {
+    btn.classList.remove("chosen-btn");
+    btn.disabled = false;
+  });
+}
